e49e253f81c3244d931a66e5d020b6e6dd965b30
Paweł Staniek <falcontech@astraklub.pl>
2026-01-30 14:21:35 +0000

    Aplha.3

--- WME_Onion_Layers/wme_onion.meta.js
+++ WME_Onion_Layers/wme_onion.meta.js
@@ -1,17 +1,18 @@
 // ==UserScript==
 // @name                                     WME Onion Layers
 // @name:pl                                     WME Cebula
-// @version                                       2602.b
+// @version                                       Alpha.3
 // @tag                                            WME
-// @description                 Adds Polish WMS overlays from e-mapa.net to WME (works only in Poland).
-// @description:pl              Cebula ma warstwy, WME ma WMSy! Dodaje polskie nakładki WMS z e-mapa.net do WME (działa tylko w Polsce).
-// @icon            https://polska.e-mapa.net/implementation/polska/images/icon.ico
-// @match           https://*.waze.com/*/editor*
-// @match           https://*.waze.com/editor*
-// @run-at          document-idle
-// @grant           none
-// @updateURL
-// @downloadURL
-// @supportURL      https://wazepolska.pl
-// @author          FalconTech
+// @description                 Adds Polish WMS overlays from e-mapa.net to WME (works only in Poland territory).
+// @description:pl              Cebula ma warstwy, WME ma WMSy! Dodaje polskie nakładki WMS z e-mapa.net do WME.
+// @grant             none
+// @author            Falcon4Tech
+// @run-at            document-idle
+// @namespace         https://wazepolska.pl
+// @match             https://*.waze.com/editor*
+// @match             https://*.waze.com/*/editor*
+// @supportURL        https://github.com/Falcon4Tech/WME/issues
+// @icon              https://polska.e-mapa.net/implementation/polska/images/icon.ico
+// @updateURL         https://cdn.jsdelivr.net/gh/Falcon4Tech/WME@main/WME_Onion_Layers/wme_onion.meta.js
+// @downloadURL       https://cdn.jsdelivr.net/gh/Falcon4Tech/WME@main/WME_Onion_Layers/wme_onion.user.js
 // ==/UserScript==
--- WME_Onion_Layers/wme_onion.user.js
+++ WME_Onion_Layers/wme_onion.user.js
@@ -1,19 +1,20 @@
 // ==UserScript==
 // @name                                     WME Onion Layers
 // @name:pl                                     WME Cebula
-// @version                                       Alpha.2
+// @version                                       Alpha.3
 // @tag                                            WME
 // @description                 Adds Polish WMS overlays from e-mapa.net to WME (works only in Poland territory).
 // @description:pl              Cebula ma warstwy, WME ma WMSy! Dodaje polskie nakładki WMS z e-mapa.net do WME.
-// @icon            https://polska.e-mapa.net/implementation/polska/images/icon.ico
-// @match           https://*.waze.com/*/editor*
-// @match           https://*.waze.com/editor*
-// @run-at          document-idle
-// @grant           none
-// @updateURL
-// @downloadURL
-// @supportURL      https://wazepolska.pl
-// @author          FalconTech
+// @grant             none
+// @author            Falcon4Tech
+// @run-at            document-idle
+// @namespace         https://wazepolska.pl
+// @match             https://*.waze.com/editor*
+// @match             https://*.waze.com/*/editor*
+// @supportURL        https://github.com/Falcon4Tech/WME/issues
+// @icon              https://polska.e-mapa.net/implementation/polska/images/icon.ico
+// @updateURL         https://cdn.jsdelivr.net/gh/Falcon4Tech/WME@main/WME_Onion_Layers/wme_onion.meta.js
+// @downloadURL       https://cdn.jsdelivr.net/gh/Falcon4Tech/WME@main/WME_Onion_Layers/wme_onion.user.js
 // ==/UserScript==
 
 (function () {
